852935b3a32fa6549b94d1873922a72b1fbbb3e3
Zach Frohardt <zfrohardt@gmail.com>
2021-04-28 10:30:50 -0700

    completed assingment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
 
 
   //Your code here:
-
+  componentDidMount() {
+    this.handleAddTimer();
+  }
 
 
 
--- src/Timer.js
+++ src/Timer.js
@@ -8,6 +8,14 @@ class Timer extends Component {
 
   // add your code here
 
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
+  }
+
+  componentWillUnmount() {
+    this.stopClock();
+  }
+
   render() {
     const { time, color } = this.state;
     return (
